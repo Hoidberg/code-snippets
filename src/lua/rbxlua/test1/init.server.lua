ca7437797d8f1c7704c21a57e022b4bcd64f710f
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-09-15 20:34:12 -0500

    Update init.server.lua

--- src/lua/rbxlua/test1/init.server.lua
+++ src/lua/rbxlua/test1/init.server.lua
@@ -1,4 +1,4 @@
-zlocal modules = {foo=require(script.foo), bar=require(script.bar), biz=require(script.biz), baz=require(script.baz), buzz=require(script.buzz)}
+local modules = {foo=require(script.foo), bar=require(script.bar), biz=require(script.biz), baz=require(script.baz), buzz=require(script.buzz)}
 for _,v in ipairs(modules) do
   print(v)
   wait(1)
